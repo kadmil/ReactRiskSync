5f54dc279511798afe1cd8eb0cb405e0acc9deff
Victor Suzdalev <victor.suzdalev@gmail.com>
2015-09-28 12:12:03 +0300

    Repaired styles for short assessment

--- src/Components/assessment-short.jsx
+++ src/Components/assessment-short.jsx
@@ -3,6 +3,6 @@ import style from '../../styles/assessment-short'
 import { ButtonStates } from '../Domain/button-states'
 
 export default function AssessmentShort (props) {
-  const assessment = props.assessment.map((group, index) => (<div key={index} className={ButtonStates[group.score]}>{group.shortLabel}</div>))
+  const assessment = props.assessment.map((group, index) => (<div key={index} className={`short ${ButtonStates[group.score]}`}>{group.shortLabel}</div>))
   return (<div className='assessment-short'>{assessment}</div>)
 }
